743cfc5af329992202c7775841a243ac995945df
Chang <c.li1@lumc.nl>
2025-07-08 10:05:49 +0200

    Cleanup

--- src/SettingsAction.cpp
+++ src/SettingsAction.cpp
@@ -1,23 +1,9 @@
 #include "SettingsAction.h"
 
 SettingsAction::SettingsAction(QObject* parent) :
-    GroupAction(parent, "SettingsAction", true),
-    _currentIterationAction(this, "Value"),
-    _maxVelocityAction(this, "Maximum velocity", 0.001f, 0.1f, 0.02f, 0.02f),
-    _numberOfIterationsAction(this, "Number of iterations", 0, 1000, 100),
-    _startAnalysisAction(this, "Start analysis")
+    GroupAction(parent, "SettingsAction", true)
+    
 {
-    setText("Example Settings");
+    setText("Settings");
 
-    _currentIterationAction.setEnabled(false);
-
-    _currentIterationAction.setToolTip("Current iteration index");
-    _maxVelocityAction.setToolTip("The speed at which the points move");
-    _numberOfIterationsAction.setToolTip("Number of iterations to compute");
-    _startAnalysisAction.setToolTip("Start the analysis");
-
-    addAction(&_currentIterationAction);
-    addAction(&_maxVelocityAction);
-    addAction(&_numberOfIterationsAction);
-    addAction(&_startAnalysisAction);
 }
